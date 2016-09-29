537b65aa60bddff1b8a99bf785820dd75d0d6107
IgorXY <igor_tonko@mail.ru>
2016-09-29 16:37:08 +0300

    Test Weak Link
    Added tests Weak link when object is alive and is null

--- WeakDelegateTest/WeakDelegateTest/UnitTest1.cs
+++ WeakDelegateTest/WeakDelegateTest/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace WeakDelegateTest
     [TestClass]
     public class UnitTest1
     {
-        private TestClass testClass{ get { return new TestClass(); } }
+        private TestClass testClass{ get { return new TestClass(); } set { testClass = value; } }
 
         [TestMethod]
         public void TestSum()
@@ -26,6 +26,35 @@ namespace WeakDelegateTest
             Delegate weakReference = new WeakDelegate((Action)testWeakClass.NullFunc);
             weakReference.DynamicInvoke();
             Assert.AreEqual(testWeakClass.IntValue, 1);
+        }
+
+        private event Action testEvent;
+
+        [TestMethod]
+        public void TestWeakLink()
+        {
+            testEvent = null;
+            var testWeakClass = testClass;
+            WeakDelegate weakDelegate = new WeakDelegate((Action)testWeakClass.NullFunc);
+            testEvent += (Action)weakDelegate;
+            testEvent.DynamicInvoke();
+            GC.Collect();
+            testEvent.DynamicInvoke();
+            Assert.IsTrue(weakDelegate.IsAlive);
+        }
+
+        [TestMethod]
+        public void TestWeakLinkObjectNull()
+        {
+            testEvent = null;
+            var testWeakClass = testClass;
+            WeakDelegate weakDelegate = new WeakDelegate((Action)testWeakClass.NullFunc);
+            testEvent += (Action)weakDelegate;
+            testEvent.DynamicInvoke();
+            testWeakClass = null;
+            GC.Collect();
+            testEvent.DynamicInvoke();
+            Assert.IsFalse(weakDelegate.IsAlive);
         }
 
     }
